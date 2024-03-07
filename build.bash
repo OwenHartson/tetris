e5d01514490d34f046ad88e7622d621ccfe7f286
Owen Hartson <oh193424@pennwest.edu>
2024-03-07 12:35:35 -0500

    first commit

---
+++ build.bash
@@ -0,0 +1,4 @@
+g++ main.cpp \
+src/utilT.h src/utilT.cpp \
+-lraylib -lGL -lm -lpthread -ldl -lrt -lX11 \
+-g -O3 -Wpedantic -Wall -Wextra -Wmisleading-indentation -Wunused -Wuninitialized -Wshadow -Wconversion -std=c++20
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <raylib.h>
+
+using namespace std;
+
+int main(){
+    
+
+
+    return 0;
+}
